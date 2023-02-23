6488d20c0c1a26839ea68e445315fe7a3bcae0fb
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-02-23 16:49:40 +0200

    add to store isRefreshing

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,8 +1,10 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
+const getIsRefreshing = state => state.auth.isRefreshing;
 const getUsername = state => state.auth.user.name;
 
 const authSelectors = {
     getIsLoggedIn,
+    getIsRefreshing,
     getUsername,
 }
 
